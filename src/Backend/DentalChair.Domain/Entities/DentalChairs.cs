8669c30c9748690f9fdcca465bc197042c1a5a1a
victorbiguete <victorbiguete@gmail.com>
2026-01-19 20:24:57 -0300

    feat: Criação das Entidades EntityBase, DentalChairs e Allocation

---
+++ src/Backend/DentalChair.Domain/Entities/DentalChairs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalChair.Domain.Entities
+{
+    public class DentalChairs : EntityBase
+    {
+        public string ChairNumber { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime PurchaseDate { get; set; }
+        public DateTime LastMaintenance { get; set; }
+
+        public virtual ICollection<> Allocations { get; set; }
+    }
+}
