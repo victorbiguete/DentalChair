948874369d3d5eb545ff751e5611c043dc393633
victorbiguete <victorbiguete@gmail.com>
2026-01-21 02:27:49 -0300

    feat: Criado o Repository de Allocation

--- src/Backend/DentalChair.Infrastructure/DataAccess/Repositories/Allocation/AllocationRepository.cs
+++ src/Backend/DentalChair.Infrastructure/DataAccess/Repositories/Allocation/AllocationRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DentalChair.Domain.IRepository.Allocation;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace DentalChair.Infrastructure.DataAccess.Repositories.Allocation
 {
-    public class AllocationRepository
+    public class AllocationRepository : IReadOnlyAllocationRepository, IWriteOnlyAllocationRepository, IUpdateOnlyAllocationRepository
     {
+        private readonly AppDbContext _context;
+
+        public AllocationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Domain.Entities.Allocation allocation) => await _context.Allocations.AddAsync(allocation);
+
+        public void Delete(Domain.Entities.Allocation allocation) => _context.Allocations.Remove(allocation);
+
+        public async Task<IList<Domain.Entities.Allocation>> GetAllAllocationAsync() => await _context.Allocations.AsNoTracking().Where(a => a.Active).ToListAsync();
+
+        public async Task<IList<Domain.Entities.Allocation>> GetByDateRangeAsync(DateTime start, DateTime end) => await _context.Allocations.AsNoTracking().Where(a => a.Active && a.StartDate >= start && a.EndDate <= end).ToListAsync();
+
+        public async Task<Domain.Entities.Allocation?> GetByDentalChairIdAsync(long id) => await _context.Allocations.AsNoTracking().Where(a => a.DentalChairId == id).FirstOrDefaultAsync();
+
+        public async Task<IList<Domain.Entities.Allocation>> GetByPatienceNameAsync(string patientName) => await _context.Allocations.AsNoTracking().Where(a => a.PatientName.Equals(patientName)).ToListAsync();
+
+        async Task<Domain.Entities.Allocation?> IReadOnlyAllocationRepository.GetByIdAsync(long id) => await _context.Allocations.AsNoTracking().Where(a => a.Id == id).FirstOrDefaultAsync();
+
+        async Task<Domain.Entities.Allocation?> IUpdateOnlyAllocationRepository.GetByIdAsync(long id) => await _context.Allocations.Where(a => a.Id == id).FirstOrDefaultAsync();
+
+        public void Update(Domain.Entities.Allocation allocation) => _context.Allocations.Update(allocation);
+
+        //public async Task<bool> IsChairAvailableAsync() => await _context.Allocations.AsNoTracking().Where(a => a.Active &&);
     }
 }
